5a9fc3fe0b2147cad9148ec6fd01c04e381f298e
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-03-18 17:13:08 +0200

    uibuild

--- index.js
+++ index.js
@@ -35,9 +35,9 @@ const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
 }
 
-/* app.get('/', (request, response) => {
+app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
-}) */
+})
 
 app.get('/api/notes', (request, response) => {
     Note.find({}).then((notes) => {
