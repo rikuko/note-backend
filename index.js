28ec0b53220d82f57433d68e2ef82f192c49a567
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-03-18 17:07:05 +0200

    uibuild

--- index.js
+++ index.js
@@ -31,9 +31,9 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
 
-app.get('/', (request, response) => {
+/* app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
-})
+}) */
 
 app.get('/api/notes', (request, response) => {
     Note.find({}).then((notes) => {
