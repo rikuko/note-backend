9995fe29df638e7c9922afa7f3b6d7609aff1500
Riku Koskinen <riku.i.koskinen@gmail.com>
2025-04-21 14:19:22 +0300

    OSA 4 kayttäjien lisääminen

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const mongoose = require('mongoose')
 const app = express()
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 /* const cors = require('cors') */
@@ -26,8 +27,11 @@ app.use(express.json())
 app.use(express.static('dist'))
 app.use(middleware.requestLogger)
 
+//Controllers
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
+// Middleware
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
